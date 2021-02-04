b0d79fe644cc02379cacf059a398383f64b7b764
cdelonga <74030958+cdelonga@users.noreply.github.com>
2021-02-03 22:13:11 -0600

    add loops

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -57,12 +57,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -125,4 +125,24 @@ public class JobData {
         }
     }
 
+    // Return the values based off a specific search term that user enters
+    public static ArrayList<HashMap<String, String>> findByValue(String searchTerm) {
+        // Loads the CSV file to loop though
+        loadData();
+        // Holds the jobs with the search term
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        // Loops thought all list
+        for (HashMap<String, String> row : allJobs) {
+            // Loop though everything contained in the dictionary
+            for (String fields : row.values()) {
+                //if the search term is contained
+                if (fields.toLowerCase().contains(searchTerm)) {
+                    jobs.add(row);
+                    break; // prevents duplicates from showing up in the array list
+                }
+            }
+        }
+        // Return the Array that contains jobs with thee search terms
+        return jobs;
+    }
 }
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+         // Loops through everything in the array
+        if (someJobs.size() == 0)
+            System.out.println("There are no jobs");
+        else{
+            for (int num = 0; num < someJobs.size(); num++) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> categories : someJobs.get(num).entrySet()) {
+                    System.out.println(categories.getKey() + ":" + categories.getValue());
+                }
+                System.out.println("*****\n");
+            }
+        }
     }
 }
+
+
